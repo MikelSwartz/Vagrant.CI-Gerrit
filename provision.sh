a8866866c721113c66cd20995e7a0b7ad018fccf
Mike Swartz <mikelswartz@gmail.com>
2016-12-28 07:59:29 -0500

    Added ant to the project

--- provision.sh
+++ provision.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-yum install wget -y
-yum install git -y
-#yum install java-1.7.0-openjdk 
+ANT_VER=apache-ant-1.9.7-bin.tar.gz
+ANT=apache-ant-1.9.7
+yum update
+yum install -y vim git tree wget
+
 cd /vagrant/
 if [ -f "/vagrant/gerrit-2.9.2.war" ] 
  then
@@ -11,3 +13,16 @@ if [ -f "/vagrant/gerrit-2.9.2.war" ]
 fi
 java -jar gerrit-2.9.2.war init -d /srv/gerrit < gerrit.answer.file
 /srv/gerrit/bin/gerrit.sh restart
+
+if [ -f "/vagrant/$ANT_VER" ]
+then
+        echo "$ANT_VER exists"
+else
+        wget http://www.us.apache.org/dist/ant/binaries/$ANT_VER
+fi
+
+tar xvfvz /vagrant/$ANT_VER -C /opt
+ln -s /opt/$ANT /opt/ant
+sh -c 'echo ANT_HOME=/opt/ant >> /etc/environment'
+ln -s /opt/ant/bin/ant /usr/bin/ant
+
