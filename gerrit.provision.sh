df5b0df43b7272ae08e6d9aa5623d345e64b4993
Mike Swartz <mikelswartz@gmail.com>
2016-12-28 13:18:48 -0500

    Added self ip id function with dig

--- gerrit.provision.sh
+++ gerrit.provision.sh
@@ -3,6 +3,10 @@ ANT_VER=apache-ant-1.9.7-bin.tar.gz
 ANT=apache-ant-1.9.7
 yum update
 yum install -y vim git tree wget
+yum install bind-utils -y #for dig
+
+mkdir -p /root/git
+MY_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
 
 cd /vagrant/
 if [ -f "/vagrant/gerrit-2.9.2.war" ] 
@@ -12,7 +16,12 @@ if [ -f "/vagrant/gerrit-2.9.2.war" ]
   wget http://gerrit-releases.storage.googleapis.com/gerrit-2.9.2.war
 fi
 java -jar gerrit-2.9.2.war init -d /srv/gerrit < gerrit.answer.file
+
+sed -i "s/gerrit.test.com:8080/${MY_IP}:8010/" /srv/gerrit/etc/gerrit.config
+
+
 /srv/gerrit/bin/gerrit.sh restart
+
 
 if [ -f "/vagrant/$ANT_VER" ]
 then
@@ -25,4 +34,5 @@ tar xvfvz /vagrant/$ANT_VER -C /opt
 ln -s /opt/$ANT /opt/ant
 sh -c 'echo ANT_HOME=/opt/ant >> /etc/environment'
 ln -s /opt/ant/bin/ant /usr/bin/ant
+
 
