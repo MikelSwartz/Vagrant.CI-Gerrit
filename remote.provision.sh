674497e6d7ac3d2d9523db2a2b294f1abba0562d
Mike Swartz <mikelswartz@gmail.com>
2016-12-29 08:07:38 -0500

    Added jenkins to remote host

--- remote.provision.sh
+++ remote.provision.sh
@@ -16,3 +16,24 @@ ln -s /opt/$ANT /opt/ant
 sh -c 'echo ANT_HOME=/opt/ant >> /etc/environment'
 ln -s /opt/ant/bin/ant /usr/bin/ant
 
+
+if [ -f /vagrant/jenkins2 ]
+then
+        echo jenkins2 exists
+else
+        mkdir /vagrant/jenkins2
+        cd /vagrant/jenkins2
+        git clone https://github.com/g0t4/jenkins2-course-spring-boot.git
+        cd
+fi
+
+yum install -y java-1.8.0-openjdk.x86_64 java-1.8.0-openjdk-devel.x86_64
+
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+yum install -y jenkins
+wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+yum install -y apache-maven
+JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk.x86_64
+service jenkins start
+
