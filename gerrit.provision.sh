c91a8d45afe714444e05bf017ec851f3fd751ab0
Mike Swartz <mikelswartz@gmail.com>
2016-12-29 06:37:09 -0500

    Moved ant to the remote host

--- gerrit.provision.sh
+++ gerrit.provision.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-ANT_VER=apache-ant-1.9.7-bin.tar.gz
-ANT=apache-ant-1.9.7
 yum update
 yum install -y vim git tree wget
 yum install bind-utils -y #for dig
@@ -21,18 +19,4 @@ sed -i "s/gerrit.test.com:8080/${MY_IP}:8010/" /srv/gerrit/etc/gerrit.config
 
 
 /srv/gerrit/bin/gerrit.sh restart
-
-
-if [ -f "/vagrant/$ANT_VER" ]
-then
-        echo "$ANT_VER exists"
-else
-        wget http://www.us.apache.org/dist/ant/binaries/$ANT_VER
-fi
-
-tar xvfvz /vagrant/$ANT_VER -C /opt
-ln -s /opt/$ANT /opt/ant
-sh -c 'echo ANT_HOME=/opt/ant >> /etc/environment'
-ln -s /opt/ant/bin/ant /usr/bin/ant
-
 
--- remote.provision.sh
+++ remote.provision.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 yum update
 yum install -y vim git tree wget
+ANT_VER=apache-ant-1.9.7-bin.tar.gz
+ANT=apache-ant-1.9.7
+
+if [ -f "/vagrant/$ANT_VER" ]
+then
+        echo "$ANT_VER exists"
+else
+        wget http://www.us.apache.org/dist/ant/binaries/$ANT_VER
+fi
+
+tar xvfvz /vagrant/$ANT_VER -C /opt
+ln -s /opt/$ANT /opt/ant
+sh -c 'echo ANT_HOME=/opt/ant >> /etc/environment'
+ln -s /opt/ant/bin/ant /usr/bin/ant
 
